8e36abffb54019d806c4384641b1df969b8d00e2
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-04-28 23:01:22 +0200

    fix call show

--- src/pages/ApartmentDetailPage.vue
+++ src/pages/ApartmentDetailPage.vue
@@ -1,13 +1,15 @@
 <script>
 import AppLoader from '../components/AppLoader.vue';
 import ApartmentCard from '../components/apartments/ApartmentCard.vue';
+import ApartmentShow from '../components/apartments/ApartmentShow.vue';
+
 import { store } from '../data/store';
 import axios from 'axios';
 const endpoint = 'http://localhost:8000/api/apartments/';
 
 export default {
     name: 'ApartmentDetailPage',
-    components: { ApartmentCard, AppLoader },
+    components: { ApartmentShow, AppLoader },
     data: () => ({
         apartment: null,
         store,
@@ -29,7 +31,7 @@ export default {
 
 <template>
     <AppLoader v-if="store.isLoading && !apartment" />
-    <ApartmentCard v-if="!store.isLoading && apartment" :apartment="apartment" :isDetail="true" />
+    <ApartmentShow v-if="!store.isLoading && apartment" :apartment="apartment" :isDetail="true" />
 
 </template>
 
--- src/routers/web.js
+++ src/routers/web.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
-
+import ApartmentDetailPage from '../pages/ApartmentDetailPage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage, name: 'home' },
+        { path: '/apartments/:id', component: ApartmentDetailPage, name: 'apartment-detail' },
         { path: '/notfound', component: NotFoundPage, name: 'not-found' },
         { path: '/:pathMatch(.)', redirect: '/notfound' }
 
