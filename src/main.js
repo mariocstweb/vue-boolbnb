99d167f63bc95a5fd27b6beaa0ac0629d8a912e8
LucaB98 <149897840+LucaB98@users.noreply.github.com>
2024-04-28 19:43:03 +0200

    add font boolbnb

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 // Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import AppLoader from './components/AppLoader.vue'
-import { router } from './routers/web.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fontsource/roboto'
 import '@fontsource-variable/manrope';
 import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-// Solid Icon
-import { faMagnifyingGlass, } from '@fortawesome/free-solid-svg-icons'
-// Brands Icon
-import { } from '@fortawesome/free-brands-svg-icons'
-// Regular Icon
-import { } from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// // Solid Icon
+// import { faMagnifyingGlass, } from '@fortawesome/free-solid-svg-icons'
+// // Brands Icon
+// import { } from '@fortawesome/free-brands-svg-icons'
+// // Regular Icon
+// import { } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faMagnifyingGlass)
 
+import { router } from './routers/web.js'
 
 const app = createApp(App)
-app.component('FontAwesomeIcon', FontAwesomeIcon, 'AppLoader', AppLoader)
+app.component('AppLoader', AppLoader)
 app.mount('#app')
 
 
